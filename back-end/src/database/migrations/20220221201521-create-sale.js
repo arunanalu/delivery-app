de4e0217f114c0090761a350065d48beab102706
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2022-02-21 18:03:49 -0300

    :sparkles: config model and migration of sale

---
+++ back-end/src/database/migrations/20220221201521-create-sale.js
@@ -0,0 +1,57 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("sales", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      total_price: {
+        type: Sequelize.DECIMAL,
+      },
+      delivery_address: {
+        type: Sequelize.STRING,
+      },
+      delivery_number: {
+        type: Sequelize.STRING,
+      },
+      sale_date: {
+        type: Sequelize.DATE,
+      },
+      status: {
+        type: Sequelize.STRING,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      seller_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      createdAt: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('sales');
+  }
+};
