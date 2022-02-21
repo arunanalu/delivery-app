1bf9d26d404ec9768faa32018ac5ddde04a0292c
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2022-02-21 17:11:54 -0300

    :sparkles: config migration and model of product

---
+++ back-end/src/database/migrations/20220221200053-create-product.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING(100)
+      },
+      price: {
+        type: Sequelize.DECIMAL(4,2)
+      },
+      url_image: {
+        type: Sequelize.STRING(200)
+      },
+      createdAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('products');
+  }
+};
---
+++ back-end/src/database/models/product.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+  const product = sequelize.define(
+    "product",
+    {
+      name: DataTypes.STRING(100),
+      price: DataTypes.DECIMAL(4,2),
+      url_image: DataTypes.STRING(200),
+    },
+    {
+      timestamps: false,
+      tableName: "products",
+    }
+  );
+  product.associate = (models) => {
+    product.hasMany(models.salesProduct, { foreignKey: "product_id", as: "sales" });
+  };
+  return product;
+};
