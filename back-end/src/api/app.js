65d8c86780d9e74aed61852336b2d98217ceb376
Mayu <satorimayu@gmail.com>
2022-02-22 18:57:45 -0300

    feat- add route to register sales/salesProducts

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const errorMiddleware = require('../middlewares/errorMiddleware');
 const loginRoute = require('../routes/loginRoute');
 const registerRouter = require('../routes/registerRoute');
+const salesRoute = require('../routes/salesRoute');
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 
 app.use('/login', loginRoute);
 app.use('/register', registerRouter);
+app.use('/sale', salesRoute);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
