53dc0b9d6899b608db9e4eb15638f39811b5bc43
mariananbp <mariananbp@gmail.com>
2022-02-23 18:58:27 -0300

    feat: header - creating header

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,8 +1,53 @@
 import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { user } from '../app/slices/userSlice';
+import '../styles/nav-bar.css';
 
 export default function Header() {
+  const history = useHistory();
+
+  const handleLogout = async () => {
+    await localStorage.removeItem('authentication');
+    history.push('/'); // verificar
+  };
+
   return (
-    <div>
-    </div>
+    <nav className="nav-bar-header">
+      <div className="container-nav-bar">
+        <Link
+          data-testid="customer_products__element-navbar-link-products"
+          className="nav-bar-products"
+          to="/products" // verificar
+        >
+          PRODUTOS
+        </Link>
+        <Link
+          data-testid="customer_products__element-navbar-link-orders"
+          className="nav-bar-orders"
+          to="/orderders" // verificar
+        >
+          MEUS PEDIDOS
+
+        </Link>
+      </div>
+      <div className="container-nav-bar">
+        <h1
+          className="nav-bar-user"
+        >
+          {user.name}
+
+        </h1>
+        <button
+          type="button"
+          className="nav-bar-logout"
+          onClick={ async () => handleLogout() }
+        >
+          Sair
+
+        </button>
+
+      </div>
+
+    </nav>
   );
 }
