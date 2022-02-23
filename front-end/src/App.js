2db79f24a494b724aac626f77b9820540dc71c8d
Marcos Estevao <dusdoom.dev@gmail.com>
2022-02-23 18:13:23 -0300

    refactor: :art: isolated react query client

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { QueryClientProvider } from 'react-query';
 import Register from './pages/Register';
 import './App.css';
 import Products from './pages/Products';
+import queryClient from './react-query/queryClient';
 
 function App() {
   return (
     <Switch>
-      <Route exact path={ ['/', '/home'] } />
-      <Route path="/register">
-        <Register />
-      </Route>
-      <Route path="/customer/products">
-        <Products />
-      </Route>
+      <QueryClientProvider client={ queryClient }>
+        <Route exact path={ ['/', '/home'] } />
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/customer/products">
+          <Products />
+        </Route>
+      </QueryClientProvider>
     </Switch>
   );
 }
