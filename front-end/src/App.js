38b4fe54dc4d8677c3bd7c309dd2c6c420849c09
Marcos Estevao <dusdoom.dev@gmail.com>
2022-02-23 13:15:37 -0300

    refactor: :lipstick: removed app css and and added products route

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Register from './pages/Register';
 import './App.css';
+import Products from './pages/Products';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Trybe!</h1>
-      <Switch>
-        <Route exact path={ ['/', '/home'] } />
-        <Route path="/register">
-          <Register />
-        </Route>
-      </Switch>
-    </div>
+    <Switch>
+      <Route exact path={ ['/', '/home'] } />
+      <Route path="/register">
+        <Register />
+      </Route>
+      <Route path="/customer/products">
+        <Products />
+      </Route>
+    </Switch>
   );
 }
 
