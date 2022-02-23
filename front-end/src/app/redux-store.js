88e7192a069b514d767f738994d4c3eeba6df05a
mariananbp <mariananbp@gmail.com>
2022-02-23 20:18:40 -0300

    remove userSlice

--- front-end/src/app/redux-store.js
+++ front-end/src/app/redux-store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartSlice from './slices/cartSlice';
-import userSlice from './slices/userSlice';
 
 export const store = configureStore({
   reducer: {
-    user: userSlice,
     cart: cartSlice,
   },
 });
