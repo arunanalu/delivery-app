9805d6ee0f772eb30a74f57cb7bbcd4afd05773c
Marcos Estevao <dusdoom.dev@gmail.com>
2022-02-23 18:12:41 -0300

    feat: :sparkles: add Product page

---
+++ front-end/src/pages/Products.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './styles/products.scss';
+import { useQuery } from 'react-query';
+import { Redirect } from 'react-router-dom';
+import { getAllProducts } from '../services/calls';
+import ProductCard from '../components/ProductCard';
+import useLocalStorage from '../hooks/useLocalStorage';
+import queryClient from '../react-query/queryClient';
+
+// coloquei aqui por causa do linter
+const fetchProducts = async (user) => {
+  const response = await getAllProducts(user.token);
+  return response.data;
+};
+
+export default function Products() {
+  const [user] = useLocalStorage('user', {});
+
+  const {
+    data: productList,
+    isError: productsFetchFailed,
+    isLoading: productsIsFetching,
+  } = useQuery('products', () => fetchProducts(user));
+
+  if (productsIsFetching) return <p>Estou carregando aaa</p>;
+  if (productsFetchFailed) {
+    queryClient.cancelQueries('products');
+    return <Redirect to="/" />;
+  }
+  return (
+    <>
+      {/* trocar pelo header componentizado */}
+      <header>
+        <nav>
+          <button
+            type="button"
+          >
+            Produtos
+          </button>
+          <button type="button">Pedidos</button>
+        </nav>
+        <h1>{user.name}</h1>
+        <button type="button">Sair</button>
+      </header>
+      <main>
+        {productList.map((product) => (
+          <ProductCard
+            key={ product.id }
+            id={ product.id }
+            name={ product.name }
+            imagePath={ product.image }
+            price={ product.price }
+          />
+        ))}
+      </main>
+    </>
+  );
+}
