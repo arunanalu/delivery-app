eba64667af7cc62a81b8bd1a3560cadc282bb1d2
Marcos Estevao <dusdoom.dev@gmail.com>
2022-02-23 17:18:34 -0300

    refactor: :bug: swapped user from redux  to localStorage

--- front-end/src/app/redux-store.js
+++ front-end/src/app/redux-store.js
@@ -1,9 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userSlice from './slices/userSlice';
 
 export const store = configureStore({
   reducer: {
-    user: userSlice,
   },
 });
 
