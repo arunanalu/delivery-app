1d9f2a086eecff0a4c57d8eea7146e5ec3a90307
mariananbp <mariananbp@gmail.com>
2022-03-04 19:04:21 -0300

    feat: order details page

--- front-end/src/pages/OrderDetails.jsx
+++ front-end/src/pages/OrderDetails.jsx
@@ -1,32 +1,43 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
-import { getSaleById } from '../services/calls';
+import { getSaleById, updateStatus } from '../services/calls';
+import useLocalStorage from '../hooks/useLocalStorage';
+import Button from '../components/Button';
+import Table from '../components/Table';
 
-const fetchOrderDetails = async (id) => {
-  const response = await getSaleById(id);
-  console.log(response)
+const fetchOrderDetails = async (id, token) => {
+  const response = await getSaleById(id, token);
   return response.data;
 };
 
 export default function OrderDetails() {
   const { id } = useParams();
+  const [user] = useLocalStorage('user', {});
   const columns = [
     'Item', 'Descrição', 'Quantidade', 'Valor Unitário', 'Sub-total'];
   const { data, isLoading, isError } = useQuery(
-    'orderDetails', () => fetchOrderDetails(id),
+    'orderDetails', () => fetchOrderDetails(id, user.token),
   );
-  console.log(data);
 
+  const handleStatus = async () => {
+    const status = {
+      status: 'entregue',
+    };
+
+    await updateStatus(id, status, user.token);
+  };
+
+  if (isLoading) return <div>Carregando</div>;
+  if (isError) return <div>Deu ruim</div>;
   return (
     <div>
-
-      <h1>Detalhes do Pedido</h1>
-      {/* <div>
+      <h1>Detalhe do Pedido</h1>
+      <div>
         <p
           data-testid="customer_order_details__element-order-details-label-order-id"
         >
-          {`PEDIDO ${order.id}`}
+          {`PEDIDO ${data.id}`}
 
         </p>
         <p
@@ -38,102 +49,37 @@ export default function OrderDetails() {
         <p
           data-testid="customer_order_details__element-order-details-label-order-date"
         >
-          {order.saleDate}
+          {data.saleDate}
 
         </p>
         <p
           data-testid="
             customer_order_details__element-order-details-label-delivery-status"
         >
-          {order.status}
+          {data.status}
 
         </p>
         <Button
+          testid="customer_order_details__button-delivery-check"
+          label="MARCAR COMO ENTREGUE"
           onClick={ handleStatus }
         />
-      </div> */}
+      </div>
+      <Table
+        columns={ columns }
+        items={ data.products }
+        testIdNumber="customer_order_details__element-order-table-item-number-"
+        testIdName="customer_order_details__element-order-table-name-"
+        testIdQuantity="customer_order_details__element-order-table-quantity-"
+        testIdUnitPrice="customer_order_details__element-order-table-unit-price-"
+        testIdSubTotal="customer_order_details__element-order-table-sub-total-"
+        thereIsButton={ false }
+      />
+      <p
+        data-testid="customer_order_details__element-order-total-price"
+      >
+        {`Total: ${data.totalPrice}`}
+      </p>
     </div>
   );
 }
-
-/* import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import Header from '../components/Header';
-import '../styles/checkout.css';
-import { getSaleById } from '../services/calls';
-import Table from '../components/Table';
-import Button from '../components/Button';
-
-export default function OrderDetails() {
-  const { id } = useParams();
-  const [order, setOrder] = useState({});
-  const columns = [
-    'Item', 'Descrição', 'Quantidade', 'Valor Unitário', 'Sub-total'];
-
-  useEffect(async () => {
-    const orderDet = await getSaleById(id);
-    setOrder(orderDet);
-  }, [id]);
-
-  const handleStatus = async () => {
-    const status = {
-      status: 'entregue',
-    };
-
-    await updateStatus(id, status);
-  };
-
-  return (
-    <div>
-      <Header />
-      <div>
-        <h1>Detalhes do Pedido</h1>
-        <div>
-          <p
-            data-testid="customer_order_details__element-order-details-label-order-id"
-          >
-            {`PEDIDO ${order.id}`}
-
-          </p>
-          <p
-            data-testid="customer_order_details__element-order-details-label-seller-name"
-          >
-            P. Vend: Fulana Pereira
-
-          </p>
-          <p
-            data-testid="customer_order_details__element-order-details-label-order-date"
-          >
-            {order.saleDate}
-
-          </p>
-          <p
-            data-testid="
-            customer_order_details__element-order-details-label-delivery-status"
-          >
-            {order.status}
-
-          </p>
-          <Button
-            onClick={ handleStatus }
-          />
-        </div>
-        <Table
-          columns={ columns }
-          items={ order.products }
-          onClick={ () => removeItem(product.id) }
-          testIdNumber="customer_order_details__element-order-table-item-number-"
-          testIdName="customer_order_details__element-order-table-name-"
-          testIdQuantity="customer_order_details__element-order-table-quantity-"
-          testIdUnitPrice="customer_order_details__element-order-table-unit-price-"
-          testIdSubTotal="customer_order_details__element-order-table-sub-total-"
-        />
-        <p
-          data-testid="customer_order_details__element-order-total-price"
-        >
-          {`Total: ${order.totalPrice}`}
-        </p>
-      </div>
-    </div>
-  );
-} */
