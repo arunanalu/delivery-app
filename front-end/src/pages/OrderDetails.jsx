2c3258199084a07a1bea581c2df481d032057453
marcosebsilva <dusdoom.dev@gmail.com>
2022-03-16 10:58:18 -0300

    feat:style added orders details header info styling

--- front-end/src/pages/OrderDetails.jsx
+++ front-end/src/pages/OrderDetails.jsx
@@ -4,6 +4,7 @@ import { useQuery } from 'react-query';
 import { getSaleById, updateStatus } from '../services/calls';
 import useLocalStorage from '../hooks/useLocalStorage';
 import Table from '../components/Table';
+import './styles/ordersDetails.css';
 
 const fetchOrderDetails = async (id, token) => {
   const response = await getSaleById(id, token);
@@ -39,64 +40,73 @@ export default function OrderDetails() {
   if (isLoading) return <div>Carregando</div>;
   if (isError) return <div>Deu ruim</div>;
   return (
-    <div>
-      <h1>Detalhe do Pedido</h1>
-      <div>
+    <div
+      className="c-order-details"
+    >
+      <h1 className="order-details__page-title">Detalhe do Pedido</h1>
+      <main>
+        <div
+          className="order-details__header"
+        >
+          <p
+            data-testid={ `${DATA_ID_DETAILS}__element-order-details-label-order-id` }
+            className="order-details__order-id"
+          >
+            {`PEDIDO ${data.id}`}
+          </p>
+          <p
+            data-testid={ `${DATA_ID_DETAILS}__element-order-details-label-seller-name` }
+          >
+            P. Vend: Fulana Pereira
+          </p>
+          <p
+            data-testid={ `${DATA_ID_DETAILS}__element-order-details-label-order-date` }
+            className="order-details__date"
+          >
+            {`${day}/${month}/${year}`}
+
+          </p>
+          <p
+            data-testid={
+              `${DATA_ID_DETAILS}__element-order-details-label-delivery-status`
+            }
+            className="order-details__status"
+          >
+            {data.status}
+
+          </p>
+          <button
+            type="button"
+            data-testid="customer_order_details__button-delivery-check"
+            onClick={ handleStatus }
+            className="order-details__shipped-btn"
+            disabled={
+              !!(data.status === 'Entregue'
+              || data.status === 'Pendente'
+              || data.status === 'Preparando')
+            }
+          >
+            MARCAR COMO ENTREGUE
+
+          </button>
+        </div>
+        <Table
+          columns={ columns }
+          items={ data.products }
+          testIdNumber="customer_order_details__element-order-table-item-number-"
+          testIdName="customer_order_details__element-order-table-name-"
+          testIdQuantity="customer_order_details__element-order-table-quantity-"
+          testIdUnitPrice="customer_order_details__element-order-table-unit-price-"
+          testIdSubTotal="customer_order_details__element-order-table-sub-total-"
+          thereIsButton={ false }
+        />
         <p
-          data-testid={ `${DATA_ID_DETAILS}__element-order-details-label-order-id` }
+          className="order-details__total"
+          data-testid="customer_order_details__element-order-total-price"
         >
-          {`PEDIDO ${data.id}`}
-
+          {`Total: ${data.totalPrice.toString().replace('.', ',')}`}
         </p>
-        <p
-          data-testid={ `${DATA_ID_DETAILS}__element-order-details-label-seller-name` }
-        >
-          P. Vend: Fulana Pereira
-
-        </p>
-        <p
-          data-testid={ `${DATA_ID_DETAILS}__element-order-details-label-order-date` }
-        >
-          {`${day}/${month}/${year}`}
-
-        </p>
-        <p
-          data-testid={
-            `${DATA_ID_DETAILS}__element-order-details-label-delivery-status`
-          }
-        >
-          {data.status}
-
-        </p>
-        <button
-          type="button"
-          data-testid="customer_order_details__button-delivery-check"
-          onClick={ handleStatus }
-          disabled={
-            !!(data.status === 'Entregue'
-            || data.status === 'Pendente'
-            || data.status === 'Preparando')
-          }
-        >
-          MARCAR COMO ENTREGUE
-
-        </button>
-      </div>
-      <Table
-        columns={ columns }
-        items={ data.products }
-        testIdNumber="customer_order_details__element-order-table-item-number-"
-        testIdName="customer_order_details__element-order-table-name-"
-        testIdQuantity="customer_order_details__element-order-table-quantity-"
-        testIdUnitPrice="customer_order_details__element-order-table-unit-price-"
-        testIdSubTotal="customer_order_details__element-order-table-sub-total-"
-        thereIsButton={ false }
-      />
-      <p
-        data-testid="customer_order_details__element-order-total-price"
-      >
-        {`Total: ${data.totalPrice.toString().replace('.', ',')}`}
-      </p>
+      </main>
     </div>
   );
 }
