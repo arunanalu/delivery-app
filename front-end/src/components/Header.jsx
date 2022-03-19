9e7e9725bdee58a69f41d005a8cb37d25defa103
Mayu <satorimayu@gmail.com>
2022-03-19 09:20:40 -0300

    :lipstick: Update header style

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import { Link, useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { removeAllFromCart, updateTotal } from '../app/slices/cartSlice';
 import useLocalStorage from '../hooks/useLocalStorage';
+
 import './styles/header.css';
 
-export default function Header() {
+export default function Header({ isSeller, isAdmin }) {
   const history = useHistory();
   const [user] = useLocalStorage('user', {});
+  const dispatch = useDispatch();
 
   const handleLogout = () => {
     localStorage.removeItem('user');
+    localStorage.removeItem('cart');
+    dispatch(removeAllFromCart());
+    dispatch(updateTotal());
     history.push('/'); // verificar
   };
 
-  return (
-    <nav className="nav-bar-header">
+  const productsAndOrders = () => (
+    <>
       <div className="container-nav-bar-products">
         <Link
           data-testid="customer_products__element-navbar-link-products"
@@ -32,6 +40,29 @@ export default function Header() {
           MEUS PEDIDOS
         </Link>
       </div>
+    </>
+  );
+
+  const sellerOrders = () => (
+    <div className="container-nav-bar-orders">
+      <Link
+        className="nav-bar-products"
+        to={ `/${user.role}/orders` }
+      >
+        PEDIDOS
+      </Link>
+    </div>
+  );
+
+  const admin = () => (
+    <p />
+  );
+
+  const sellerOrCostumer = () => (isSeller ? sellerOrders() : productsAndOrders());
+
+  return (
+    <nav className="nav-bar-header">
+      { isAdmin ? admin() : sellerOrCostumer() }
       <div className="container-nav-bar-user">
         <h1
           className="nav-bar-user"
@@ -52,3 +83,13 @@ export default function Header() {
     </nav>
   );
 }
+
+Header.propTypes = {
+  isSeller: PropTypes.bool,
+  isAdmin: PropTypes.bool,
+};
+
+Header.defaultProps = {
+  isSeller: undefined,
+  isAdmin: undefined,
+};
