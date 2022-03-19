c462d9bba97af7e0472c8147434cca540648fc99
Mayu <satorimayu@gmail.com>
2022-03-19 09:19:56 -0300

    :lipstick: Update administrator code style

--- front-end/src/components/AdministratorCards.jsx
+++ front-end/src/components/AdministratorCards.jsx
@@ -1,42 +1,90 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+import useLocalStorage from '../hooks/useLocalStorage';
+import { deleteUser } from '../services/calls';
+import './styles/administratorCards.css';
 
-export default function AdministratorCards() {
+export default function AdministratorCards({ attUsersFunc }) {
   const data = useSelector(({ user }) => user.users);
+  const [user] = useLocalStorage('user');
+
+  const fetchDeleteUser = async (id) => {
+    await deleteUser(id, user.token);
+    await attUsersFunc();
+  };
+
+  const categories = ['ID', 'Nome', 'Email', 'Tipo', 'Deletar'];
+
   return (
-    <div>
-      {data.length !== 0 && data.map(({
-        id, name, email, role,
-      }, index) => (
-        <div key={ index }>
-          <span
-            data-testid={ `admin_manage__element-user-table-item-number-${index}` }
-          >
-            {id}
-          </span>
-          <span
-            data-testid={ `admin_manage__element-user-table-name-${index}` }
-          >
-            {name}
-          </span>
-          <span
-            data-testid={ `admin_manage__element-user-table-email-${index}` }
-          >
-            {email}
-          </span>
-          <span
-            data-testid={ `admin_manage__element-user-table-role-${index}` }
-          >
-            {role}
-          </span>
-          <button
-            type="button"
-            data-testid={ `admin_manage__element-user-table-remove-${index}` }
-          >
-            Excluir
-          </button>
-        </div>
-      ))}
-    </div>
+    <>
+      {/* <div className="users-main-ct">
+        {data.length !== 0 && data.map(({
+          id, name, email, role,
+        }, index) => (
+          <div key={ index } className="users-ct">
+            <span
+              data-testid={ `admin_manage__element-user-table-item-number-${index}` }
+            >
+              {id}
+            </span>
+            <span
+              data-testid={ `admin_manage__element-user-table-name-${index}` }
+            >
+              {name}
+            </span>
+            <span
+              data-testid={ `admin_manage__element-user-table-email-${index}` }
+            >
+              {email}
+            </span>
+            <span
+              data-testid={ `admin_manage__element-user-table-role-${index}` }
+            >
+              {role}
+            </span>
+            <button
+              type="button"
+              data-testid={ `admin_manage__element-user-table-remove-${index}` }
+              onClick={ () => fetchDeleteUser(id) }
+            >
+              Excluir
+            </button>
+          </div>
+        ))}
+      </div> */}
+      <table className="users-main-ct">
+        <tr className="users-titles">
+          {
+            categories.map((element) => (
+              <th key={ element }>{element}</th>
+            ))
+          }
+        </tr>
+        {
+          data.length !== 0 && data.map((userRegistered) => (
+            <tr key={ userRegistered.id } className="users-lines">
+              <td>{userRegistered.id}</td>
+              <td>{userRegistered.name}</td>
+              <td>{userRegistered.email}</td>
+              <td>{userRegistered.role}</td>
+              <td className="delete-user-ct-btn">
+                <button
+                  type="button"
+                  className="delete-user-button"
+                  onClick={ () => fetchDeleteUser(userRegistered.id) }
+                >
+                  Excluir
+                </button>
+              </td>
+            </tr>
+          ))
+        }
+      </table>
+    </>
   );
 }
+
+AdministratorCards.propTypes = {
+  attUsersFunc: PropTypes.func.isRequired,
+};
--- front-end/src/pages/Administrator.jsx
+++ front-end/src/pages/Administrator.jsx
@@ -1,15 +1,18 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useCallback, useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import Input from '../components/Input';
 import Select from '../components/Select';
 import Button from '../components/Button';
 import { validRegisterForm } from '../utils/validations/schemas';
-import { addUser, getAllUsersOnLoad } from '../app/slices/userSlice';
+import { getAllUsersOnLoad } from '../app/slices/userSlice';
 import { getAllUsers, postUserAdm } from '../services/calls';
 import AdministratorCards from '../components/AdministratorCards';
+import './styles/administrator.css';
+import Header from '../components/Header';
 
 export default function Administrator() {
-  const roles = ['seller', 'customer'];
+  const roles = ['seller', 'customer', 'administrator'];
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -25,11 +28,11 @@ export default function Administrator() {
     setTokenInit(token);
     const { data } = await getAllUsers(tokenInit);
     dispatch(getAllUsersOnLoad(data));
-  }, [dispatch, tokenInit]);
+  }, []);
 
   useEffect(() => {
     fetchAndSetToken();
-  }, [fetchAndSetToken]);
+  }, []);
 
   const isFormValid = () => {
     const { error } = validRegisterForm.validate({
@@ -49,7 +52,8 @@ export default function Administrator() {
         password,
       };
       await postUserAdm(tokenInit, user);
-      dispatch(addUser(user));
+      // dispatch(addUser(user));
+      await fetchAndSetToken();
     } catch (error) {
       setErrorMessage(error);
       setShowErrorMessage(true);
@@ -57,50 +61,62 @@ export default function Administrator() {
   };
 
   return (
-    <div style={ { display: 'flex', flexDirection: 'column' } }>
-      <Input
-        type="text"
-        label="Name"
-        onChange={ ({ target }) => setName(target.value) }
-        value={ name }
-        testid="admin_manage__input-name"
-      />
-      <Input
-        type="text"
-        label="Email"
-        onChange={ ({ target }) => setEmail(target.value) }
-        value={ email }
-        testid="admin_manage__input-email"
-        id="admin_manage__input-email"
-      />
-      <Input
-        type="text"
-        label="Senha"
-        onChange={ ({ target }) => setPassword(target.value) }
-        value={ password }
-        testid="admin_manage__input-password"
-        id="admin_manage__input-password"
-      />
-      <Select
-        testid="admin_manage__select-role"
-        onChange={ ({ target }) => setRole(target.value) }
-        label="Tipo"
-        value={ role }
-        options={ roles }
-      />
-      <Button
-        testid="admin_manage__button-register"
-        label="Cadastrar"
-        disabled={ !isFormValid() }
-        onClick={ onClicAddUser }
-      />
-      {showErrorMessage && (
-        <span data-testid="admin_manage__element-invalid-register">
-          {errorMessage}
-        </span>
-      )}
-      <br />
-      <AdministratorCards />
-    </div>
+    <>
+      <Header isAdmin />
+      <div
+        style={ { display: 'flex', flexDirection: 'column' } }
+        className="main-admin-ct"
+      >
+        <h2>Cadastrar usuários</h2>
+        <div className="register-user">
+          <div className="inputs-admin-ct">
+            <Input
+              type="text"
+              label="Name:"
+              onChange={ ({ target }) => setName(target.value) }
+              value={ name }
+              testid="admin_manage__input-name"
+            />
+            <Input
+              type="text"
+              label="Email:"
+              onChange={ ({ target }) => setEmail(target.value) }
+              value={ email }
+              testid="admin_manage__input-email"
+              id="admin_manage__input-email"
+            />
+            <Input
+              type="text"
+              label="Senha:"
+              onChange={ ({ target }) => setPassword(target.value) }
+              value={ password }
+              testid="admin_manage__input-password"
+              id="admin_manage__input-password"
+            />
+            <Select
+              testid="admin_manage__select-role"
+              onChange={ ({ target }) => setRole(target.value) }
+              label="Tipo: "
+              value={ role }
+              options={ roles }
+            />
+          </div>
+          <Button
+            testid="admin_manage__button-register"
+            label="Cadastrar"
+            disabled={ !isFormValid() }
+            onClick={ onClicAddUser }
+          />
+        </div>
+        {showErrorMessage && (
+          <span data-testid="admin_manage__element-invalid-register">
+            {errorMessage}
+          </span>
+        )}
+        <br />
+        <h2>Lista de usuários</h2>
+        <AdministratorCards attUsersFunc={ fetchAndSetToken } />
+      </div>
+    </>
   );
 }
