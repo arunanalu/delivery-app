377748dba33bd964876207a4429265d95ab73560
Mayu <satorimayu@gmail.com>
2022-03-20 15:03:15 -0300

    small change

--- back-end/src/api/index.js
+++ back-end/src/api/index.js
@@ -14,19 +14,18 @@ app.use(express.json());
 
 app.use(cors());
 
+app.get('/coffee', (_req, res) => res.status(418).end());
 app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
 app.use('/login', loginRoute);
 app.use(userRouter);
 app.use('/product', productRouter);
 app.use('/sale', salesRoute);
 
-app.get('/coffee', (_req, res) => res.status(418).end());
-
 app.use(errorMiddleware);
 
-// const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 
-// app.listen(port);
-// console.log(`Api rodando na porta ${port}`);
+app.listen(port);
+console.log(`Api rodando na porta ${port}`);
 
 module.exports = app;
