c03e47f722b42ff7f125c24f5485e6b52efbd2f3
Mayu <satorimayu@gmail.com>
2022-03-20 15:17:18 -0300

    fix deta deploy

--- back-end/index.js
+++ back-end/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
-const errorMiddleware = require('../middlewares/errorMiddleware');
-const loginRoute = require('../routes/loginRoute');
-const userRouter = require('../routes/userRoute');
-const productRouter = require('../routes/productRouter');
-const salesRoute = require('../routes/salesRoute');
+// const path = require('path');
+const errorMiddleware = require('./src/middlewares/errorMiddleware');
+const loginRoute = require('./src/routes/loginRoute');
+const userRouter = require('./src/routes/userRoute');
+const productRouter = require('./src/routes/productRouter');
+const salesRoute = require('./src/routes/salesRoute');
 require('dotenv').config();
 
 const app = express();
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
-app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
+// app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
 app.use('/login', loginRoute);
 app.use(userRouter);
 app.use('/product', productRouter);
